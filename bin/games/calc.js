dcb9958a14d3bf3613e86af38770c0e60fbe75cb
Alex Arts <a_arz@mail.ru>
2022-06-29 21:31:21 +0400

    создана и отлажена игра brain-calc

---
+++ bin/games/calc.js
@@ -0,0 +1,18 @@
+import gameEngine from '../../src/index.js';
+
+const questionText = 'What is the result of the expression?';
+
+const isCalcGame = () => {
+  const a = Math.floor(Math.random() * 100);
+  const b = Math.floor(Math.random() * 100);
+  const operations = [a+b, a-b, a*b];
+  const signs = ['+','-','*'];
+
+  const randomIndex = Math.floor(Math.random() * 2)
+  const target = operations[randomIndex].toString();
+  const randomOperation = `${a}${signs[randomIndex]}${b}`;
+
+  return [randomOperation, target];
+};
+
+export default () => gameEngine(questionText, isCalcGame); 
