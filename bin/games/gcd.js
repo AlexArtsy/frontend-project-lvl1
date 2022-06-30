f3fb23392812c10565d61a5c45600b2b14205134
Alex Arts <a_arz@mail.ru>
2022-06-30 14:08:18 +0400

    добавлена обработка события, когда одно из чисел = 0

--- bin/games/gcd.js
+++ bin/games/gcd.js
@@ -1,25 +1,25 @@
 import gameEngine from '../../src/index.js';
 
 const questionText = 'Find the greatest common divisor of given numbers.';
-const getMinNumber = (a, b) => {
-  if (Math.abs(a) < Math.abs(b)) {
-    return a;
-  }
-  return b;
-};
 
 const gcdGame = () => {
   const a = Math.floor(Math.random() * 100);
   const b = Math.floor(Math.random() * 100);
+
+  if (a === 0 || b === 0) {
+    gcdGame();
+  }
+
   const questionContent = `${a} ${b}`;
   let target;
 
-  for (let i = getMinNumber(a, b); i > 0; i -= 1) {
+  for (let [i] = [a, b].sort(); i > 0; i -= 1) {
     if (a % i === 0 && b % i === 0) {
       target = i.toString();
       break;
     }
   }
+  console.log(target);
   return [questionContent, target];
 };
 
