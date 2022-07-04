1be1a1c3b4bc1e2764ff20805b0ca60939fd99f0
Alex Arts <a_arz@mail.ru>
2022-07-04 16:45:20 +0400

    fix bug in progression.js

--- bin/games/progression.js
+++ bin/games/progression.js
@@ -5,12 +5,11 @@ const questionText = 'What number is missing in the progression?';
 const progressionGame = () => {
   const startNum = Math.floor(Math.random() * 100);
   const stepOfProg = Math.floor(Math.random() * 9) + 1;
-  const lengthOfProgression = Math.floor(Math.random() * 5) + 5;
-  let target = Math.floor(Math.random() * lengthOfProgression);
+  let target = Math.floor(Math.random() * 10);
   let nextNum = startNum;
   let randomProgression = '';
 
-  for (let i = 0; i < lengthOfProgression; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     if (target === i) {
       randomProgression += ' .. ';
       target = nextNum;
